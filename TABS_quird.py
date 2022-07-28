af7b962822608e50c38bfced4edbd54af41b945f
Safi <safi.sharieff@gmail.com>
2022-07-28 20:57:58 +0400

    28-07-2022 Chnages
    - Streamlined the GUI structure.
    - Fixed a bug that gave each button the same properties
    - Fixed a minor bug in the JSON formatting of the project

--- TABS_quird.py
+++ TABS_quird.py
@@ -1,67 +1,63 @@
 import json
 import tkinter
 from tkinter import Tk
-from tkinter import Button
+from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 from tkinter import ttk
 import os
+import time
 
 
-def overload(x, filename):
-    print(x, filename)
-    move1 = json.loads('{"m_modID": -1, "m_ID": 1998224969 }')
-    move2 = json.loads('{"m_modID": -1, "m_ID": 702611234 }')
-    biglist = [move1, move1, move1, move2, move1, move1]
-    x["m_combatMoves"] = json.dumps(biglist)
+recordbook = {}
+
+
+def overload(m):
+    global recordbook
+    filename = recordbook[m]
+
+    with open(filename, encoding = 'utf-8') as file:
+        things = json.loads(file.read())
+        things["m_combatMoves"] = 0
+        move1 = json.loads('{"m_modID": -1, "m_ID": 1998224969 }')
+        move2 = json.loads('{"m_modID": -1, "m_ID": 702611234 }')
+        biglist = [move1, move1, move1, move2, move1, move1]
+        things["m_combatMoves"] = json.dumps(biglist)
+        print(things, m, filename)
     
 
 def generateFrames(folder_path, top):
-
-    """
-    Button(win, text="Button-1",height= 5, width=10).pack()
-    Button(win, text="Button-2",height=8, width=15).pack()
-    Button(win, text= "Button-3",height=10, width=30).pack()
-    """
+    global recordbook
     
-    for filename in os.listdir(folder_path):
+    for i, filename in enumerate(os.listdir(folder_path)):
         f = os.path.join(folder_path, filename)
-        print(f)
-        if os.path.isfile(f):
+        if os.path.isfile(f) and filename.lower().endswith('.unit'):
+            print(f)
             with open(f, encoding = 'utf-8') as file:
                 things = json.loads(file.read())
-                Button(top, text=things["m_name"], command= lambda:overload(things, filename), height=83, width=180).pack()
-                
+                recordbook[things["m_name"]] = f
+                Button(top, text=things["m_name"], command= lambda m=things["m_name"]:overload(m), height=2, width=20).pack()
 
 
 def main():
 
     root = Tk()
-    container = ttk.Frame(root)
-    canvas = tkinter.Canvas(container)
-    scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
-    scrollable_frame = ttk.Frame(canvas)
+    container = Frame(root).pack()
+    canvas = tkinter.Canvas(root)
+    canvas.configure(scrollregion=canvas.bbox("all"))
+    scrollbar = Scrollbar(canvas, orient="vertical", command=canvas.yview)
+    canvas.pack()
+    canvas["yscrollcommand"] = scrollbar.set
     
     
     dirname = ''
     while dirname == '':
         #root.withdraw()
         dirname = askdirectory()
-        generateFrames(dirname, root)
+        generateFrames(dirname, canvas)
 
-    root.geometry("180x250")
-    root.mainloop()    
-
-    """
-    with open(filename, encoding = 'utf-8') as file:
-        things = json.loads(file.read())
-        things["m_combatMoves"] = 0
-        overload(things)
-        print(things)
-    print("here")
     root.geometry("180x250")
     root.mainloop()
-    """
 
 
 if __name__=="__main__":
